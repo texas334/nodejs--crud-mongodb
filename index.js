4f5c0ee0bedcc0de1129f2556730cf4802e6cb0a
ROSHU <104186405+texas334@users.noreply.github.com>
2022-10-11 07:58:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
  const cors = require('cors'); 
   
  const { mongoose } = require('./db.js'); 
- var employeeController = require('./controllers/employeeController.js'); 
+  
   
  var app = express(); 
  app.use(bodyParser.json()); 
@@ -11,5 +11,4 @@ const express = require('express');
   
  app.listen(3000, () => console.log('Server started at port : 3000')); 
   
-  
- app.use('/employees', employeeController);
+ 
